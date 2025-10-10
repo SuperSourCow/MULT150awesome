afd22bee99f96e0640949a55824e93680f8037fd
SuperSourCow <sourcow420@gmail.com>
2025-10-09 20:41:00 -0700

    created scripts for the TIY scene using print and the Update built-in, then created more scripts for the hour7_p1 scene using arithmetic operations to create PlayerHealth

---
+++ Jrivera_Hour7_P1/Assets/Scripts/HelloWorldScript.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+public class HelloWorldScript : MonoBehaviour
+{ 
+    //Use this for initilization
+    void Start ()
+    {
+        print("Hello World");
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+    }
+}
