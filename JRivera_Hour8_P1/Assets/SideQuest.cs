bb94057c528c8ea978a3f07ced742bcb4615fbf6
SuperSourCow <sourcow420@gmail.com>
2025-10-26 23:13:19 -0700

    added TIY scene and Hour8 P1 scene, created multiple scripts and game objects, a moving camera, and a toggable light source

---
+++ JRivera_Hour8_P1/Assets/SideQuest.cs
@@ -0,0 +1,7 @@
+
+/* 1. Method name is TakeDamage */
+/* 2. It returns an int variable type.
+ * 3. The method has one parameter: int damageAmount
+   4. The code is:
+        int health = 100;
+        return health - damageAmount*/
