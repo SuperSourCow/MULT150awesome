0605a2c41841349eb88a9715df134ad193c9605a
SuperSourCow <sourcow420@gmail.com>
2025-11-16 19:25:13 -0800

    created Chaos Ball game, annotated every extra addition to GameCritic.cs

---
+++ JRivera_Hour10/Assets/GameCritic.cs
@@ -0,0 +1,9 @@
+//Is the game too easy or too hard? Holy christ its difficult
+//What would make it easier or harder? Reduced speed probably
+//What would give it a "wow" factor? Different ball properties,like one ball is crazy quick, but another you HAVE to push cuz its heavy.
+//What parts of the game are fun? What parts of the game are tedious? I enjoy the fast-paced action, but actually managing the balls can get a bit frustrating.
+
+// Added ball variations, made the Green and Red balls into "bullets", a faster. slimmer object, both have 75 speed. Increased the size of the Orange Ball and reduced its speed to 10. Blue ball remains the same.
+// Changed the size of the bumper to a scale of X:3, Y:2, Z:1.
+// Changed Bumper into a hand, so we can slap em.
+// Added terrain to cover water, and added blank buildings outside arena.
