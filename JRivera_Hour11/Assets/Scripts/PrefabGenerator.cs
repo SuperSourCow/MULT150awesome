6a9baf6872f3be0a1e38be8b4e95b835ff3ef4fb
SuperSourCow <sourcow420@gmail.com>
2025-11-23 22:41:00 -0800

    created the 10 in a row instantiation script for Hour11 scene

--- JRivera_Hour11/Assets/Scripts/PrefabGenerator.cs
+++ JRivera_Hour11/Assets/Scripts/PrefabGenerator.cs
@@ -5,24 +5,22 @@ using UnityEngine;
 public class PrefabGenerator : MonoBehaviour
 {
     public GameObject prefab;
+    public int numberOfPrefabs = 10;
+    public float spawnOffset = 2.0f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        for (int i = 0; i < numberOfPrefabs; i++)
+        {
+            Vector3 spawnPosition = new Vector3(i * spawnOffset, 0, 0);
+            Instantiate (prefab, spawnPosition, Quaternion.identity);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.B))
-        {
-            Instantiate(prefab);
-        }
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            Instantiate(prefab, transform.position, transform.rotation);
-        }
 
     }
 }
