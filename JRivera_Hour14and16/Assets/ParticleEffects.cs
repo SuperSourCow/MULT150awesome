72a9b782832625dec9553de9b971a241b831bfce
SuperSourCow <sourcow420@gmail.com>
2025-11-30 23:16:18 -0800

    created 3 scenes, two linked with builder and game UI, one with particle effects. changes are recorded in scripts

---
+++ JRivera_Hour14and16/Assets/ParticleEffects.cs
@@ -0,0 +1,7 @@
+//Changed Simulation Speed to 20.
+//Changed color to orange.
+//Changed 3D Start Size Y to 3.
+//Changed Max Particles to 100.
+//Messed with the Size by Speed system a bit, making the particles slightly smaller.
+
+//Added a cube and renamed to "Fireworks"
---
+++ JRivera_Hour14and16/Assets/UserInterface.cs
@@ -0,0 +1,5 @@
+// Added Macondo font and renamed "Game" title.
+// Changed panel background to black.
+// Changed idle button background to gray, and selected color to white.
+// Changed pressing button color to black.
+// Changed title font to be italic.
