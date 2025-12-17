f90e35a06c6ccb4cf0accc2455ea2a874120d87d
SuperSourCow <sourcow420@gmail.com>
2025-12-16 20:16:13 -0800

    Created Gauntlet runner game, modified obstacles and powerups to be different. GameCritic script details changes

---
+++ JRivera_Hour20/Assets/GameCritic.cs
@@ -0,0 +1,19 @@
+
+//Is the game too easy or too hard? 
+// - It definitely varies. It is reliant on the obstacle spawns, but the phasing helps. Overall, its very middle of the road in difficulty.
+
+//What changes would you do to make it easier or harder?
+// - To make things a bit more manageable, I'd add more types of power-ups, like one that slows the environment but not the player.
+// - To make it harder, I'd add more diverse obstacles of varying speeds and shapes, potentially covering other incoming obstacles.
+
+//What would make the game more appealing?
+// - Crazier power-ups and obstacles, as well as different stages of difficulty as time passes, like increasing speed or more frequent obstacles.
+
+//What parts of the game are fun?
+// - Encountering a pattern of obstacles that are difficult to dodge, since it forces you to use the phase ability.
+
+//What parts of the game are tedious?
+// - The game gets repetitive after a bit, and could use more shake-up.
+
+//-Remade the Powerup to be an Energy Drink (albeit a bit buggy ingame).
+//-Remade the Obstacle to be a Tub of Glue, added a drywall-looking texture to it. https://www.pexels.com/photo/white-wall-paint-1260727/ <- Image used for texture.
