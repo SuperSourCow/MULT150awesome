e5c22429ed35e76d67ba1a8f0f15abbd3b1e1bba
SuperSourCow <sourcow420@gmail.com>
2025-12-18 20:59:55 -0800

    modified gauntlet runner, added oiia hazard, added donut collectible. Added start scene with all modifications listed, added bg music to both scenes

---
+++ JRivera_Final/Assets/Modifications.cs
@@ -0,0 +1,55 @@
+﻿public class Modifications
+{/*
+    ░▒▓████████▓▒░▒▓█▓▒░▒▓███████▓▒░ ░▒▓██████▓▒░░▒▓█▓▒░        
+    ░▒▓█▓▒░      ░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░        
+    ░▒▓█▓▒░      ░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░        
+    ░▒▓██████▓▒░ ░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓████████▓▒░▒▓█▓▒░        
+    ░▒▓█▓▒░      ░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░        
+    ░▒▓█▓▒░      ░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░        
+    ░▒▓█▓▒░      ░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓████████▓▒░ */
+
+
+    /*  ================================================================
+        GAUNTLET RUNNER
+        ================================================================
+        1 - directional light (colour, intensity, etc)
+        modifications: Changed color to a light red, X rotation to 35, intensity to 1.5, and changed shadow strength to 0.84.
+
+        2 - fog
+        modifications: Made fog red, exponential, density of 0.1
+
+        3 - create & apply skybox and customise it (atmosphere thickness, sky tint, exposure, etc)
+        modifications: Red color, with dark red ground. Sun size of 0.125, Convergence of 4.39, Atmosphere thickness of 0.36, Exposure of 2.25.
+        
+        OBSTACLE PREFAB
+        4 - add or modify light component (colour, range, intensity, etc)
+        modifications: Changed color to white, and intensity to 2.
+        
+        POWERUP PREFAB
+        5 - replace with a new complex object comprised of 3 or more shapes (different from previously made object)
+        modifications: Made 2 batteries, 3 cylinders each with different textures. Tilted one battery to add style.
+
+        6 - use gameobject hierarchy to keep the powerup prefab organised
+        [no record necessary]
+
+        7 - apply at least 2 new textures
+        (add source url or package name)
+        texture source 1: Aluminum Foil - https://www.pexels.com/photo/crumpled-plastic-wrapper-6144430/
+        texture source 2: Metal Surface - https://www.pexels.com/photo/brown-stain-on-a-rough-surface-2850515/
+
+        8 - add particle system component and customise at least 3 parameters
+        parameter 1: Changed renderer mesh to a cube, gave it the "Powerup" material to make it yellow. Sizes of the cubes are xyz 0.3
+        parameter 2: Changed shape to donut, radius of 3, radius of .2
+        parameter 3: Changed emission rate over time to 30.
+
+        WALL
+        9 - convert a wall to prefab, create a variant, and use the variant to replace the other wall in the scene
+        [no record necessary]
+
+        10 - modify wall prefab material tint so that the 2 walls are different in appearance or colour
+        [no record necessary]
+     
+     */
+
+
+}
